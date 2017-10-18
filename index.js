d62b8ca2ea5083acce6542c191143c1fcb0ff152
Carlos Osiel Rojas Velázquez <osiel8809@gmail.com>
2017-10-17 23:00:52 -0400

    Add app configuration

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express')
+    , app = module.exports = express()
+    , path = require('path')
+    , mongodb = require('mongodb')
+    , router = require('./api/routes');
+
+/**
+ * Some configurations for the express app.
+ */
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+
+//setup a database from ENV variable named MONGODB_CONNECTION
+if (!process.env.MONGODB_CONNECTION) {
+    throw new Error('The database connection environment variable not exist, please add using export MONGODB_CONNECTION')
+}
+mongodb.connect(process.env.MONGODB_CONNECTION, (err, db) => {
+    if(err) {
+        throw err;
+    }
+    app.set('db', db);
+});
+
+app.use('/', router);
+
+//Run the application in port 3000
+//TODO cambiar el puerto al 3000
+app.listen(3010, () => console.log('Application is running on http://127.0.0.1:3000'));
